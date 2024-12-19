1c083ea91a62e454109ed30daea40da02a6da06c
Agnish1611 <sukla.baruipur2@gmail.com>
2024-12-19 20:10:03 +0530

    feat: project setup

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import { BrowserRouter, Routes } from "react-router";
+
+export const App = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+      </Routes>
+    </BrowserRouter>
+  );
+};
